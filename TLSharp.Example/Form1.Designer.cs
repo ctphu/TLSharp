d0e72376ed5e0dd8c8f3b612c233797abab128df
ctphu <phu.t.cao@outlook.com>
2021-03-30 09:51:44 +0700

    Get Channel Message

--- TLSharp.Example/Form1.Designer.cs
+++ TLSharp.Example/Form1.Designer.cs
@@ -41,103 +41,119 @@
             this.label4 = new System.Windows.Forms.Label();
             this.grLogin = new System.Windows.Forms.GroupBox();
             this.grMessage = new System.Windows.Forms.GroupBox();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.tbReceipt = new System.Windows.Forms.TextBox();
             this.grLogin.SuspendLayout();
             this.grMessage.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(243, 115);
+            this.button1.Location = new System.Drawing.Point(294, 71);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 47);
-            this.button1.TabIndex = 0;
+            this.button1.Size = new System.Drawing.Size(159, 39);
+            this.button1.TabIndex = 4;
             this.button1.Text = "SEND";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.btnSendMessage_Click);
             // 
             // txtMessage
             // 
-            this.txtMessage.Location = new System.Drawing.Point(18, 89);
+            this.txtMessage.Location = new System.Drawing.Point(11, 87);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4);
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(311, 20);
-            this.txtMessage.TabIndex = 1;
+            this.txtMessage.Size = new System.Drawing.Size(264, 22);
+            this.txtMessage.TabIndex = 3;
             // 
             // txtNumber
             // 
-            this.txtNumber.Location = new System.Drawing.Point(18, 70);
+            this.txtNumber.Location = new System.Drawing.Point(11, 40);
+            this.txtNumber.Margin = new System.Windows.Forms.Padding(4);
             this.txtNumber.Name = "txtNumber";
-            this.txtNumber.Size = new System.Drawing.Size(199, 20);
-            this.txtNumber.TabIndex = 1;
-            this.txtNumber.Text = "+84906841595";
+            this.txtNumber.Size = new System.Drawing.Size(264, 22);
+            this.txtNumber.TabIndex = 0;
+            this.txtNumber.Text = "+855974980728";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(243, 54);
+            this.button2.Location = new System.Drawing.Point(294, 23);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 36);
-            this.button2.TabIndex = 0;
+            this.button2.Size = new System.Drawing.Size(159, 39);
+            this.button2.TabIndex = 2;
             this.button2.Text = "1. GET CODE";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.btnGetCode_Click);
             // 
             // txtCode
             // 
-            this.txtCode.Location = new System.Drawing.Point(18, 122);
+            this.txtCode.Location = new System.Drawing.Point(11, 87);
+            this.txtCode.Margin = new System.Windows.Forms.Padding(4);
             this.txtCode.Name = "txtCode";
-            this.txtCode.Size = new System.Drawing.Size(199, 20);
+            this.txtCode.Size = new System.Drawing.Size(264, 22);
             this.txtCode.TabIndex = 1;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(243, 96);
+            this.button3.Location = new System.Drawing.Point(292, 71);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(121, 47);
-            this.button3.TabIndex = 0;
+            this.button3.Size = new System.Drawing.Size(161, 38);
+            this.button3.TabIndex = 3;
             this.button3.Text = "2. LOGIN";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // txtReceiveNumber
             // 
-            this.txtReceiveNumber.Location = new System.Drawing.Point(18, 37);
+            this.txtReceiveNumber.Location = new System.Drawing.Point(11, 40);
+            this.txtReceiveNumber.Margin = new System.Windows.Forms.Padding(4);
             this.txtReceiveNumber.Name = "txtReceiveNumber";
-            this.txtReceiveNumber.Size = new System.Drawing.Size(199, 20);
-            this.txtReceiveNumber.TabIndex = 2;
-            this.txtReceiveNumber.Text = "+84906841595";
+            this.txtReceiveNumber.Size = new System.Drawing.Size(264, 22);
+            this.txtReceiveNumber.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 54);
+            this.label1.Location = new System.Drawing.Point(8, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.Size = new System.Drawing.Size(75, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "SDT Login";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 106);
+            this.label2.Location = new System.Drawing.Point(8, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.Size = new System.Drawing.Size(160, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Telegram Code via SMS";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 24);
+            this.label3.Location = new System.Drawing.Point(8, 19);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.Size = new System.Drawing.Size(104, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Phone Receive";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 73);
+            this.label4.Location = new System.Drawing.Point(8, 66);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.Size = new System.Drawing.Size(65, 17);
             this.label4.TabIndex = 6;
             this.label4.Text = "Message";
             // 
@@ -149,34 +165,94 @@
             this.grLogin.Controls.Add(this.txtNumber);
             this.grLogin.Controls.Add(this.button3);
             this.grLogin.Controls.Add(this.button2);
-            this.grLogin.Location = new System.Drawing.Point(48, 33);
+            this.grLogin.Location = new System.Drawing.Point(13, 13);
+            this.grLogin.Margin = new System.Windows.Forms.Padding(4);
             this.grLogin.Name = "grLogin";
-            this.grLogin.Size = new System.Drawing.Size(392, 151);
-            this.grLogin.TabIndex = 7;
+            this.grLogin.Padding = new System.Windows.Forms.Padding(4);
+            this.grLogin.Size = new System.Drawing.Size(474, 129);
+            this.grLogin.TabIndex = 0;
             this.grLogin.TabStop = false;
-            this.grLogin.Text = "groupBox1";
+            this.grLogin.Text = "Auth";
             // 
             // grMessage
             // 
+            this.grMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.grMessage.Controls.Add(this.tbUserName);
+            this.grMessage.Controls.Add(this.label5);
             this.grMessage.Controls.Add(this.label4);
             this.grMessage.Controls.Add(this.label3);
             this.grMessage.Controls.Add(this.txtReceiveNumber);
             this.grMessage.Controls.Add(this.txtMessage);
             this.grMessage.Controls.Add(this.button1);
-            this.grMessage.Location = new System.Drawing.Point(48, 203);
+            this.grMessage.Location = new System.Drawing.Point(500, 13);
+            this.grMessage.Margin = new System.Windows.Forms.Padding(4);
             this.grMessage.Name = "grMessage";
-            this.grMessage.Size = new System.Drawing.Size(392, 170);
-            this.grMessage.TabIndex = 8;
+            this.grMessage.Padding = new System.Windows.Forms.Padding(4);
+            this.grMessage.Size = new System.Drawing.Size(474, 129);
+            this.grMessage.TabIndex = 1;
             this.grMessage.TabStop = false;
-            this.grMessage.Text = "groupBox2";
+            this.grMessage.Text = "Send";
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(294, 40);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(159, 22);
+            this.tbUserName.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(291, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "UserName";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btStart);
+            this.groupBox1.Controls.Add(this.tbReceipt);
+            this.groupBox1.Location = new System.Drawing.Point(12, 149);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(962, 423);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Receipt";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(12, 21);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(116, 38);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // tbReceipt
+            // 
+            this.tbReceipt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbReceipt.Location = new System.Drawing.Point(12, 65);
+            this.tbReceipt.Multiline = true;
+            this.tbReceipt.Name = "tbReceipt";
+            this.tbReceipt.Size = new System.Drawing.Size(929, 340);
+            this.tbReceipt.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 447);
+            this.ClientSize = new System.Drawing.Size(989, 584);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.grMessage);
             this.Controls.Add(this.grLogin);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -184,6 +260,8 @@
             this.grLogin.PerformLayout();
             this.grMessage.ResumeLayout(false);
             this.grMessage.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -203,6 +281,11 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox grLogin;
         private System.Windows.Forms.GroupBox grMessage;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbReceipt;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- TLSharp.Example/TelegramApi.cs
+++ TLSharp.Example/TelegramApi.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TeleSharp.TL;
+using TeleSharp.TL.Channels;
+using TeleSharp.TL.Messages;
+
 namespace TLSharp.Example
 {
     public class TelegramApi
     {
-        int apiId = 193474;
-        string apiHash = "7a74bfa34444ba1c869159957bb21a08";
-        string phoneCodeHash = "";
+        public int apiId = 193474;
+        public string apiHash = "7a74bfa34444ba1c869159957bb21a08";
+        public string phoneCodeHash = "";
         public string PhoneNumber { get; set; }
         public virtual async Task AuthUser()
         {
@@ -30,6 +33,163 @@ namespace TLSharp.Example
             return client.IsUserAuthorized();
 
         }
+        public async Task<List<TLMessage>> GatherChannelHistory(string channelName, int offset = 0, int maxId = -1, int limit = 50)
+        {
+            List<TLMessage> _resultMessages = new List<TLMessage>();
+            var client = new TLSharp.Core.TelegramClient(apiId, apiHash);
+            await client.ConnectAsync();
+
+            var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
+            var chat = dialogs.Chats.ToList()
+                .OfType<TLChannel>()
+                .FirstOrDefault(c => c.Title == channelName);
+
+            if (chat.AccessHash != null)
+            {
+                TLChannel channel = new TLChannel() { Id = chat.Id, AccessHash = chat.AccessHash };
+
+                var request = new TLRequestGetFullChannel();
+                
+                var fullChat = await client.SendRequestAsync<TLRequestGetFullChannel>(request);
+                //var request = new TLRequestGetFullChat() { ChatId = chat.Id };
+                //var fullChat = await client.SendRequestAsync<TeleSharp.TL.Messages.TLChatFull>(request);
+                //var request = new TLRequestGetMessages() { Id = chat. };
+                //var fullChat = await client.SendRequestAsync<TeleSharp.TL.Channels.TLRequestGetMessages>(request);
+                //var request = new TLRequestGetAllChats() { };
+                //var fullChat = await client.SendRequestAsync<TeleSharp.TL.Messages.TLRequestGetAllChats>(request);
+
+                var tlAbsMessages =
+                    await client.GetHistoryAsync(
+                        new TLInputPeerChannel { ChannelId = chat.Id, AccessHash = chat.AccessHash.Value }, offset,
+                        maxId, limit);
+
+                var tlChannelMessages = (TLChannelMessages)tlAbsMessages;
+
+                for (var index = 0; index < tlChannelMessages.Messages.Count - 1; index++)
+                {
+                    var tlAbsMessage = tlChannelMessages.Messages.ToList()[index];
+                    var message = (TLMessage)tlAbsMessage;
+                    //Now you have the message and you can do what you need with it
+                    //the code below is an example of messages classification
+                    if (message.Media == null)
+                    {
+                        _resultMessages.Add(new TLMessage()
+                        {
+                            Id = message.Id,
+                            FromId = chat.Id,
+                            Message = message.Message,
+                            Views = message.Views,
+                        });
+                    }
+                    else
+                    {
+
+                        switch (message.Media.GetType().ToString())
+                        {
+                            case "TeleSharp.TL.TLMessageMediaPhoto":
+                                var tLMessageMediaPhoto = (TLMessageMediaPhoto)message.Media;
+
+                                _resultMessages.Add(new TLMessage()
+                                {
+                                    Id = message.Id,
+                                    FromId = chat.Id,
+                                    Message = message.Message,
+                                    Views = message.Views,
+                                    Media = message.Media,
+
+                                });
+                                break;
+                                //case "TeleSharp.TL.TLMessageMediaDocument":
+                                //    var tLMessageMediaDocument = (TLMessageMediaDocument)message.media;
+
+                                //    _resultMessages.Add(new ChannelMessage()
+                                //    {
+                                //        Id = message.id,
+                                //        ChannelId = chat.id,
+                                //        Content = tLMessageMediaDocument.caption,
+                                //        Type = EnChannelMessage.MediaDocument,
+                                //        Views = message.views ?? 0,
+                                //    });
+                                //    break;
+                                //case "TeleSharp.TL.TLMessageMediaWebPage":
+                                //    var tLMessageMediaWebPage = (TLMessageMediaWebPage)message.media;
+                                //    string url = string.Empty;
+                                //    if (tLMessageMediaWebPage.webpage.GetType().ToString() != "TeleSharp.TL.TLWebPageEmpty")
+                                //    {
+                                //        var webPage = (TLWebPage)tLMessageMediaWebPage.webpage;
+                                //        url = webPage.url;
+                                //    }
+
+                                //    _resultMessages.Add(new ChannelMessage
+                                //    {
+                                //        Id = message.id,
+                                //        ChannelId = chat.id,
+                                //        Content = message.message + @" : " + url,
+                                //        Type = EnChannelMessage.WebPage,
+                                //        Views = message.views ?? 0,
+                                //    });
+                                //    break;
+                        }
+                    }
+                }
+            }
+            return _resultMessages;
+        }
+
+        //public virtual async Task<string> GetLastMessageFromUser(string sUserName)
+        //{
+        //    string sResult;
+        //    var client = new TLSharp.Core.TelegramClient(apiId, apiHash);
+        //    await client.ConnectAsync();
+        //    var normalizedUser = (sUserName.StartsWith("@") ?
+        //        sUserName.Substring(1, sUserName.Length - 1) :
+        //        sUserName).ToLower();
+        //    var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
+        //    var user = dialogs.Users
+        //        .Where(x => x.GetType() == typeof(TLUser))
+        //        .OfType<TLUser>()
+        //        .FirstOrDefault(x => x.Username != null && x.Username.ToLower() == normalizedUser);
+
+        //    TLAbsMessages tlAbsMessages = await client.GetHistoryAsync(new TLInputPeerUser() {
+        //        UserId = user.Id,
+        //        AccessHash = user.AccessHash.Value
+        //    }, 0, -1, 100);
+        //    TLMessages tlMessages = (TLMessages)tlAbsMessages;
+        //    TLMessage lastmessage = (TLMessage)tlMessages.Messages[0];
+        //    sResult = lastmessage.FromId.ToString() + ":" + lastmessage.Message;
+        //    return sResult;
+        //}
+        public virtual async Task<TLMessage> GetLastMessageFromUser(string sUserName)
+        {
+            TLMessage lastmessage = null;
+            var client = new TLSharp.Core.TelegramClient(apiId, apiHash);
+            await client.ConnectAsync();
+            var normalizedUser = (sUserName.StartsWith("@") ?
+                sUserName.Substring(1, sUserName.Length - 1) :
+                sUserName).ToLower();
+            var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
+            var user = dialogs.Users
+                .Where(x => x.GetType() == typeof(TLUser))
+                .OfType<TLUser>()
+                .FirstOrDefault(x => x.Username != null && x.Username.ToLower() == normalizedUser);
+
+            TLAbsMessages tlAbsMessages = await client.GetHistoryAsync(new TLInputPeerUser()
+            {
+                UserId = user.Id,
+                AccessHash = user.AccessHash.Value
+            }, 0, -1, 100);
+            if (tlAbsMessages.GetType() == typeof(TLMessagesSlice))
+            {
+                var castMessages = (TLMessagesSlice)tlAbsMessages;
+                lastmessage = (TLMessage)castMessages.Messages[0];
+            }
+            else if (tlAbsMessages.GetType() == typeof(TLMessages))
+            {
+                TLMessages tlMessages = (TLMessages)tlAbsMessages;
+                lastmessage = (TLMessage)tlMessages.Messages[0];
+            }
+            return lastmessage;
+        }
         public virtual async Task MakeAuthAsync(string code)
         {
             var client = new TLSharp.Core.TelegramClient(apiId, apiHash);
@@ -40,7 +200,7 @@ namespace TLSharp.Example
         {
             var client = new TLSharp.Core.TelegramClient(apiId, apiHash);
             await client.ConnectAsync();
-            var result = await client.GetContactsAsync();
+            var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
 
             // this is because the contacts in the address come without the "+" prefix
             var normalizedNumber = toNumber.StartsWith("+") ?
@@ -49,10 +209,48 @@ namespace TLSharp.Example
 
 
 
-            var user = result.Users
+            var user = dialogs.Users
                .OfType<TLUser>()
                .FirstOrDefault(x => x.Phone == normalizedNumber);
-            await client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, message);
+            await client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id, AccessHash = user.AccessHash.Value }, message);
+        }
+        public virtual async Task SendMessageToUser(string toUserName, string message)
+        {
+            var client = new TLSharp.Core.TelegramClient(apiId, apiHash);
+            await client.ConnectAsync();
+            var normalizedUser = (toUserName.StartsWith("@") ?
+                toUserName.Substring(1, toUserName.Length - 1) :
+                toUserName).ToLower();
+            var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
+            var user = dialogs.Users
+                .Where(x => x.GetType() == typeof(TLUser))
+                .OfType<TLUser>()
+                .FirstOrDefault(x => x.Username != null && x.Username.ToLower() == normalizedUser);
+            if (user != null)
+            {
+                await client.SendTypingAsync(new TLInputPeerUser() { UserId = user.Id, AccessHash = user.AccessHash.Value });
+                await client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id, AccessHash = user.AccessHash.Value }, message);
+            }
+
+            //await client.SendMessageAsync(new TLInputPeerUser() {  UserId = user.Id, AccessHash = user.AccessHash.Value }, message);
+
+
+            //send message
+            //await client.SendMessageAsync(new TLInputPeerChannel() { ChannelId = chat.Id, AccessHash = chat.AccessHash.Value }, "OUR_MESSAGE");
+            //var result = await client.SearchUserAsync(normalizedUser, 10);
+            //if (result.Users.Count > 0)
+            //{
+            //    TLUser user = (TLUser)result.Users[0];
+            //    await client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, message);
+            //}
+            //var normalizedNumber1 = toUserName.StartsWith("@") ?
+            //    toUserName.Substring(1, toUserName.Length - 1) :
+            //    toUserName;
+
+            //var user = result.Users
+            //   .OfType<TLUser>()
+            //   .FirstOrDefault(x => x.Username == toUserName);
+            //await client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, message);
         }
     }
 }
